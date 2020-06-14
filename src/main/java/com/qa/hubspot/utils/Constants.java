2c1f2015c63e4962b92440969ec11c69d999c43a
Kavithalearn <kavithalearn@gmail.com>
2020-06-13 22:06:45 -0400

    changes in config and constants

--- src/main/java/com/qa/hubspot/utils/Constants.java
+++ src/main/java/com/qa/hubspot/utils/Constants.java
@@ -2,7 +2,7 @@ package com.qa.hubspot.utils;
 
 public class Constants {
 	
- public static final String LOGIN_PAGE_TITLE = "HubSpot Login987";
+ public static final String LOGIN_PAGE_TITLE = "HubSpot Login";
  public static final String HOME_PAGE_TITLE = "Reports dashBoard";
  public static final String HOME_PAGE_HEADER = "Sales dashBoard";
  public static final String CONTACTS_PAGE_TITLE = "Contacts";
